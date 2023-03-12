03904665bfc148029756decbf43020a01973ebce
Tharles Amaro <tharlesamaro@gmail.com>
2023-03-12 19:50:48 -0300

    update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ setup(
     description="This is a Python package that simplifies the definition of standard HTTP responses for FastAPI APIs.",
     long_description=open('README.md').read(),
     version='0.0.1',
-    setup_requires=['pytest-runner'],
     author='Tharles Amaro',
     author_email='tharlesamaro@gmail.com',
     url="https://github.com/tharlesamaro/fastapi-doc-http-response",
